fffaeb68897649856302852a2bc7d78fe8570af2
José de Jesús Zárate García <jose.jesus.zarate.garcia@outlook.com>
2025-02-23 13:55:46 -0600

    Inicio almost finished

--- src/components/Layout/NavBar.tsx
+++ src/components/Layout/NavBar.tsx
@@ -10,18 +10,18 @@ const NavBar: React.FC<NavBarProps> = ({isHome}) => {
     <nav className={"flex navbar navbar-expand-lg navbar-light bg-light justify-between p-5 pl-10 pr-10 items-center fixed top-0 left-0 w-full z-50"}>
       <a className={`navbar-brand font-extrabold text-2xl ${isHome ? 'text-black' : 'text-white'}`} href='#'>JESÚS ZÁRATE</a>
       <div className='columns-4 font-bold text-white'> 
-        <ul className='navbar-nav pr-10'>
-          <li className='nav-item'>
-            <a className='nav-link' href='#'>Home</a>
+        <ul className='pr-8'>
+          <li className='flex'>
+            <a className='w-24 hover:bg-white hover:text-black rounded-3xl text-center' href='#'>Home</a>
           </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='#'>About</a>
+          <li className='flex'>
+            <a className='w-24 hover:bg-white hover:text-black rounded-3xl text-center' href='#'>About</a>
           </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='#'>Projects</a>
+          <li className='flex'>
+            <a className='w-24 hover:bg-white hover:text-black rounded-3xl text-center' href='#'>Projects</a>
           </li>
-          <li className='nav-item'>
-            <a className='nav-link' href='#'>Contact</a>
+          <li className='flex'>
+            <a className='w-24 hover:bg-white hover:text-black rounded-3xl text-center' href='#'>Contact</a>
           </li>
         </ul>
       </div>
--- src/components/Main/Presentation.tsx
+++ src/components/Main/Presentation.tsx
@@ -1,23 +1,38 @@
 import React from 'react'
 import { BsArrowUpRightCircleFill } from "react-icons/bs";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
 
 const Presentation = () => {
   return (
     
     <div className="absolute w-full h-screen bg-transparent overflow-hidden">
         <div className="absolute w-[105%] h-[200vh] bg-[#D7D7D7] transform -translate-x-1/2 -translate-y-1/2 rotate-12 -z-50"></div> 
-        <div className='h-screen w-[50%] items-center justify-center flex flex-col'>
-            <div>
-                <h1 className='font-bold text-4xl pb-5'>Hi, I am</h1>
-                <h1 className='font-bold text-7xl pb-5'>Jesús Zárate</h1>
-                <h1 className='text-2xl pb-5'>Full-Stack Developer</h1>
-                <div className='flex'>
-                    <button className='bg-[#D3E97A] h-12 w-44 rounded-3xl font-bold flex justify-center items-center gap-4 group'>
-                        <p>CONTACT ME</p>
-                        <div className='bg-black w-2 h-2 rounded-full group-hover:hidden'></div>
-                        <div className='hidden w-2 h-2 group-hover:block'></div>
-                    </button>
+        <div className='flex justify-between'>
+            <div className='h-screen w-[50%] items-center justify-center flex flex-col'>
+                <div>
+                    <h1 className='font-bold text-4xl pb-5'>Hi, I am</h1>
+                    <h1 className='font-bold text-7xl pb-5'>Jesús Zárate</h1>
+                    <h1 className='text-2xl pb-5'>Full-Stack Developer</h1>
+                    <div className='flex gap-5 items-center'>
+                        <button className='bg-[#D3E97A] h-12 w-44 rounded-3xl font-bold flex justify-center items-center gap-4 group transition-transform duration-150 hover:scale-105 hover:cursor-pointer'>
+                            <p>CONTACT ME</p>
+                            <div className="relative w-5 h-5 flex justify-center items-center">
+                                <div className="absolute bg-black w-2 h-2 rounded-full transition-opacity duration-150 group-hover:opacity-0"></div>
+                                <BsArrowUpRightCircleFill className="bg-white rounded-full absolute w-8 h-8 opacity-0 scale-75 transition-all duration-150 group-hover:opacity-100 group-hover:scale-100"/>
+                            </div>
+                        </button>
+                        <button className='bg-[#222222] h-10 w-10 rounded-3xl font-bold flex justify-center items-center gap-4 group transition-transform duration-150 hover:scale-110 hover:cursor-pointer'>
+                            <FaLinkedinIn className='text-[#D3E97A] w-4 h-4' />
+                        </button>
+                        <button className='bg-[#222222] h-10 w-10 rounded-3xl font-bold flex justify-center items-center gap-4 group transition-transform duration-150 hover:scale-110 hover:cursor-pointer'>
+                            <FiGithub className='text-[#D3E97A] w-5 h-5' />
+                        </button>
+                    </div>
                 </div>
+            </div>
+            <div className='h-screen w-[50%] items-center justify-center flex flex-col'>
+                
             </div>
         </div>
     </div>
